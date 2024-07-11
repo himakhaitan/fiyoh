7dfe81d85e90a049cffbcba8f8581a65c98e09ba
himakhaitan <himanshukhaitan108@gmail.com>
2024-07-11 21:47:15 +0530

    Room Model Configured #25

--- lib/models/room.dart
+++ lib/models/room.dart
@@ -5,20 +5,31 @@ class Room {
   final int occupancy;
   final int floor;
   final String roomNumber;
+  final List<Map<String, String>>? tenants;
 
   Room({
     required this.roomId,
     required this.occupancy,
     required this.floor,
     required this.roomNumber,
+    this.tenants,
   });
 
   factory Room.fromDocumentSnapshot(DocumentSnapshot doc) {
+    
+    List<Map<String, String>> tenants = [];
+    doc['tenants'].forEach((value) {
+      Map<String, String> tenant = {};
+      tenant['user_id'] = value['user_id'];
+      tenant['booking_id'] = value['booking_id'];
+    });
+
     return Room(
       roomId: doc.id,
       occupancy: doc['occupancy'],
       floor: doc['floor'],
       roomNumber: doc['room_number'],
+      tenants: tenants,
     );
   }
 }
