4c2c3e7b887a0c225f53ecc733de043b13bd5d1f
himakhaitan <himanshukhaitan108@gmail.com>
2024-07-20 21:45:51 +0530

    Resolves #57

---
+++ lib/models/tenant.dart
@@ -0,0 +1,38 @@
+// Imports
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+// Tenant Class
+class Tenant {
+  final String id;
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String countryCode;
+  final String phoneNumber;
+  final String? activeBooking;
+
+  // Constructor
+  Tenant({
+    required this.id,
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.countryCode,
+    required this.phoneNumber,
+    this.activeBooking,
+  });
+
+  // Instanciate the Tenant class from a DocumentSnapshot
+  factory Tenant.fromDocumentSnapshot(DocumentSnapshot doc) {
+
+    return Tenant(
+      id: doc.id,
+      firstName: doc['first_name'],
+      lastName: doc['last_name'],
+      email: doc['email'],
+      countryCode: doc['country_code'],
+      phoneNumber: doc['phone_number'] ?? '',
+      activeBooking: doc['active_booking'],
+    );
+  }
+}
